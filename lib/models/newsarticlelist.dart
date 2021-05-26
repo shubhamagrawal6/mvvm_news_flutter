1f07b61838d292c3f739ffd3945cebeeacf36124
shubhamagrawal6 <shubhamagrawal322@gmail.com>
2021-05-27 00:04:16 +0530

    list of news articles model

---
+++ lib/models/newsarticlelist.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_news_flutter/models/newsarticle.dart';
+import 'package:mvvm_news_flutter/services/web_service.dart';
+import 'package:mvvm_news_flutter/viewmodels/news_articles_vm.dart';
+
+enum LoadingStatus { completed, searching, empty }
+
+class NewsArticleListViewModel with ChangeNotifier {
+  List<NewsArticleViewModel> articles = List.empty();
+  LoadingStatus loadingStatus = LoadingStatus.empty;
+
+  void topHeadlines() async {
+    List<NewsArticle> newsArticles = await WebService().fetchTopHeadlines();
+    loadingStatus = LoadingStatus.searching;
+    notifyListeners();
+    this.articles = newsArticles
+        .map((article) => NewsArticleViewModel(article: article))
+        .toList();
+
+    if (this.articles.isEmpty) {
+      this.loadingStatus = LoadingStatus.empty;
+    } else {
+      this.loadingStatus = LoadingStatus.completed;
+    }
+    notifyListeners();
+  }
+}
