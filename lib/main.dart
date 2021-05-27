b9233f0937fb88c1d807c8b510bc2d4b9d2b843a
shubhamagrawal6 <shubhamagrawal322@gmail.com>
2021-05-27 16:56:27 +0530

    theme colour changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'models/newsarticlelist.dart';
 import 'screens/news_screen.dart';
@@ -11,11 +12,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.light,
+        statusBarColor: Color(0xff120a8f),
+      ),
+    );
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter News App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
+        scaffoldBackgroundColor: Color(0xfffefdfd),
+        appBarTheme: AppBarTheme(
+          color: Color(0xfffefdfd),
+          elevation: 0,
+          textTheme: TextTheme(),
+          actionsIconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
       ),
       home: MultiProvider(
         providers: [
