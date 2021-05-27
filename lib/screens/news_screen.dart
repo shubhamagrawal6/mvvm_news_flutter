973056934676e0c648ea16a689e0e190d661f7e3
shubhamagrawal6 <shubhamagrawal322@gmail.com>
2021-05-27 16:56:13 +0530

    colours changed

--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -19,10 +19,38 @@ class _NewsScreenState extends State<NewsScreen> {
   @override
   Widget build(BuildContext context) {
     var listViewModel = Provider.of<NewsArticleListViewModel>(context);
-    return Scaffold(
-      appBar: AppBar(),
-      body: NewsGrid(
-        articles: listViewModel.articles,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 100,
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0),
+                child: Text(
+                  "News",
+                  style: TextStyle(
+                    fontSize: 50,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              Divider(
+                color: Color(0xff120a8f),
+                height: 8,
+                thickness: 8,
+                indent: 10,
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            Icon(Icons.more_vert),
+          ],
+        ),
+        body: NewsGrid(
+          articles: listViewModel.articles,
+        ),
       ),
     );
   }
