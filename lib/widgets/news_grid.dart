e168dd7be3a553cadd332b5e256724dc1423e9e3
shubhamagrawal6 <shubhamagrawal322@gmail.com>
2021-05-27 16:55:15 +0530

    added ontap to send to details page

--- lib/widgets/news_grid.dart
+++ lib/widgets/news_grid.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_news_flutter/screens/news_article_details.dart';
 import 'package:mvvm_news_flutter/viewmodels/news_articles_vm.dart';
 import 'package:mvvm_news_flutter/widgets/circle_image.dart';
 
 class NewsGrid extends StatelessWidget {
   final List<NewsArticleViewModel> articles;
   const NewsGrid({this.articles});
+  void _showArticleDetails(context, article) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) {
+          return NewsArticleDetailScreen(article: article);
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,28 +23,34 @@ class NewsGrid extends StatelessWidget {
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
       ),
-      itemBuilder: (context, index) {
-        return GridTile(
-          child: Container(
-            margin: EdgeInsets.symmetric(
-              horizontal: 10,
-              vertical: 20,
-            ),
-            child: CircleImage(
-              imageUrl: articles[index].urlToImage,
-            ),
-          ),
-          footer: Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: 15,
-            ),
-            child: Text(
-              articles[index].title,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
+      itemBuilder: (_, index) {
+        var article = articles[index];
+        return GestureDetector(
+          onTap: () {
+            _showArticleDetails(context, article);
+          },
+          child: GridTile(
+            child: Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 20,
               ),
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
+              child: CircleImage(
+                imageUrl: article.urlToImage,
+              ),
+            ),
+            footer: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 15,
+              ),
+              child: Text(
+                article.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
             ),
           ),
         );
